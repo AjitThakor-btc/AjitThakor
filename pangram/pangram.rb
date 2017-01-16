e5cd173603f5ec7920af9d58fd8bc6b15412cb42
AjitThakor9191 <ajit.thakor9191@gmail.com>
2017-01-16 16:12:44 +0530

    Determine if a sentence is a pangram.

---
+++ pangram/pangram.rb
@@ -0,0 +1,22 @@
+class Pangram
+   def self.pangram?(phrase)
+     if phrase == ''
+       return false
+     end
+
+     numOfChar = 0
+     phrase = phrase.upcase
+     phrase = phrase.split('')
+
+     ('A'..'Z').each do|index|
+       if (phrase.include?index)
+        numOfChar += 1
+       end
+    end
+     if(numOfChar == 26)
+       return true
+     else
+       return false
+     end
+   end
+end
