0a2c70214d4f8861dd99029b2becfcb6c74931eb
AjitThakor9191 <ajit.thakor9191@gmail.com>
2017-01-25 11:55:53 +0530

    This is hamming program

---
+++ hamming/newhamming.rb
@@ -0,0 +1,23 @@
+class Hamming
+  def self.compute(string1,string2)
+    string1 = string1.upcase
+    string2 = string2.upcase
+    str1 = string1.split('')
+    str2 = string2.split('')
+    count = 0
+    if str1.length!=str2.length
+      raise ArgumentError
+    end
+    if str1 == '' || str2 == ''
+      return 0
+    end
+    len = string1.length
+    index = 0
+    while len >0
+      count +=1 if str1[index] != str2[index]
+      index += 1
+      len -= 1
+    end
+    return count
+  end
+end
