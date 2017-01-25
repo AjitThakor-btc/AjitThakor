2994a9f9fed8b8097643a48092041035077e25f7
AjitThakor9191 <ajit.thakor9191@gmail.com>
2017-01-25 11:47:56 +0530

    Hamming test program

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,26 +1,22 @@
 class Hamming
-
   def self.compute(string1,string2)
-    string1=string1.upcase
-    string2=string2.upcase
+    string1 = string1.upcase
+    string2 = string2.upcase
     str1 = string1.split('')
     str2 = string2.split('')
     count = 0
-
     if str1.length!=str2.length
       raise ArgumentError
     end
-
-    if str1=='' || str2==''
+    if str1 == '' || str2 == ''
       return 0
     end
-
-    i=string1.length
-    index=0
-    while i>0
-      count+=1 if str1[index]!=str2[index]
-      index+=1
-      i-=1
+    len = string1.length
+    index = 0
+    while len >0
+      count +=1 if str1[index] != str2[index]
+      index += 1
+      len -= 1
     end
     return count
   end
